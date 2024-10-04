20304ff049707ce942a8711fe0652f50f8910369
psych0wh1z <ikeagudike@gmail.com>
2024-10-04 12:01:10 +0100

    Add deployment scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 const { vars } = require("hardhat/config");
+const ACCOUNT_PRIVATE_KEY = vars.get("ACCOUNT_PRIVATE_KEY");
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-const METAMASK_PRIVATE_KEY = vars.get("METAMASK_PRIVATE_KEY");
+const LISK_RPC_URL = vars.get("LISK_RPC_URL");
 
 module.exports = {
     solidity: {
         compilers: [
+            {
+                version: "0.8.27",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
             {
                 version: "0.8.24",
                 settings: {
@@ -29,14 +39,30 @@ module.exports = {
         ],
     },
     networks: {
-        sepolia: {
+        "sepolia": {
             url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-            accounts: [METAMASK_PRIVATE_KEY],
+            accounts: [ACCOUNT_PRIVATE_KEY],
+        },
+        "lisk-sepolia": {
+            url: LISK_RPC_URL,
+            accounts: [ACCOUNT_PRIVATE_KEY],
+            gasPrice: 1000000000,
         },
     },
     etherscan: {
         apiKey: {
-            sepolia: ETHERSCAN_API_KEY,
+            "sepolia": ETHERSCAN_API_KEY,
+            "lisk-sepolia": "123",
         },
+        customChains: [
+            {
+                network: "lisk-sepolia",
+                chainId: 4202,
+                urls: {
+                    apiURL: "https://sepolia-blockscout.lisk.com/api",
+                    browserURL: "https://sepolia-blockscout.lisk.com/",
+                },
+            },
+        ],
     },
 };
